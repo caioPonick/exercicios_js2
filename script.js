9279f8620a57ad6b6ae65810329119df380bb168
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-03-20 12:39:44 -0300

    add - ex011

---
+++ script.js
@@ -0,0 +1,15 @@
+/// ex 011 
+/*
+function verificarSinal(num) {
+    if (num = 0) {
+        console.log("Seu número é 0");
+    } else if (num>0){
+        console.log("Seu número é maior que zero.")
+    } else {
+        console.log("Seu número é menor que zero.")
+    }
+}
+verificarSinal(prompt("Digite um número: "))
+*/
+
+///ex012

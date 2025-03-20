2ec26fb0254b0c8076527cf04ab0cd8b4a2a9bf1
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-03-20 12:55:18 -0300

    add - ate o ex015

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ verificarSinal(prompt("Digite um número: "))
 */
 
 ///ex012
+/*
+function converterTemperatura(celsius){
+    faren = (celsius* (9/5))+32;
+    console.log("A conversão em farenheit é " + faren);
+}
+converterTemperatura(prompt("Quanto graus está? (Celsius): "));
+*/
+
+///ex013
+/*
+let n = 0
+let soma = 0
+while(n < 100) {
+    n++;
+    soma = soma + n;
+}
+console.log(soma);
+*/
+
+///ex014
+/*
+function substituirPalavra(str){
+    console.log(str.replace("azul", "vermelho"));
+}
+substituirPalavra(prompt("Digite uma frase com azul: "));
+*/
+
+///ex015
+/*
+function verificarTamanho(str){
+    comprimento = str.length;
+    if(comprimento > 10) {
+        console.log("Sua frase tem mais de 10 caracteres!");
+    } else {
+        console.log("Sua frase tem menos de 10 caracteres!");
+    }
+}
+verificarTamanho(prompt("Digite uma frase: "));
+*/
+
+///ex016

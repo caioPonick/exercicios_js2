d1d3db1096fb1eb4197b7df48ef0f77030e4a3db
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-03-21 11:02:53 -0300

    add - ex019

--- script.js
+++ script.js
@@ -80,3 +80,10 @@ verificarAprovacao(prompt("Qual foi sua nota?"));
 */
 
 ///ex019
+for(let n = 0; n <= 30; n++){
+    if (n % 2 === 0) {
+        console.log(n + " é um número par!");
+    } else {
+        console.log(n + " é um número ímpar!");
+    }
+}
